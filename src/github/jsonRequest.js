f5cbb51e0cc51abfa7305ec6b903d8a8bb2feace
jamworld <chencha92111@gmail.com>
2017-07-01 00:07:10 -0500

    [UPDATE] jsonRequest.js
    Throw exception when the response status code is not success.

--- src/github/jsonRequest.js
+++ src/github/jsonRequest.js
@@ -18,5 +18,10 @@ export default function jsonRequest(access_token, token_type, method = "get", ur
   };
   if (data) option.body = JSON.stringify(data);
   return fetch(url + "?" + paramString, option)
-    .then(res => no_parse ? res : res.json());
+      .then(response => {
+          if (!response.ok) {
+              throw Error(response.statusText);
+          }
+          return response
+      }).then(res => no_parse ? res : res.json())
 }
