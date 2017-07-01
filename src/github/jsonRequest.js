1023306374d0a3cf12c22e9822c2c38a3460a963
jamworld <chencha92111@gmail.com>
2017-07-01 00:57:17 -0500

    [REFORMAT] jsonRequest.js

--- src/github/jsonRequest.js
+++ src/github/jsonRequest.js
@@ -4,26 +4,26 @@ import "whatwg-fetch";
 "use strict";
 export default function jsonRequest(access_token, token_type, method = "get", url, params, data = null, no_parse = false) {
 
-  if (!params) params = {};
-  let paramString = Object.keys(params).map(key => {
-    return key + "=" + params[key]
-  }).join('&');
-  let option = {
-    method: method.toUpperCase(),
-    headers: {
-      Authorization: token_type + " " + access_token,
-      Accept: "application/vnd.github.v3+json",
-      "Content-Type": "application/json; charset=utf-8"
-    },
-  };
-  if (data) option.body = JSON.stringify(data);
-  return fetch(url + "?" + paramString, option)
-  // NOTE: fetch will always success whenever the connection success
-      // .then(response => {
-      //     if (!response.ok) {
-      //         throw error(response.statustext);
-      //     }
-      //     return response
-      // })
-     .then(res => no_parse ? res : res.json())
+    if (!params) params = {};
+    let paramString = Object.keys(params).map(key => {
+        return key + "=" + params[key]
+    }).join('&');
+    let option = {
+        method: method.toUpperCase(),
+        headers: {
+            Authorization: token_type + " " + access_token,
+            Accept: "application/vnd.github.v3+json",
+            "Content-Type": "application/json; charset=utf-8"
+        },
+    };
+    if (data) option.body = JSON.stringify(data);
+    return fetch(url + "?" + paramString, option)
+    // NOTE: fetch will always success whenever the connection success
+    // .then(response => {
+    //     if (!response.ok) {
+    //         throw error(response.statustext);
+    //     }
+    //     return response
+    // })
+        .then(res => no_parse ? res : res.json())
 }
